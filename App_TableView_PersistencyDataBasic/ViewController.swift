6efe3026616e43dd924349ce0792a2418175d5b5
phdafoe <phdafoe@gmail.com>
2016-01-12 22:52:41 +0100

    Initial Commit

---
+++ App_TableView_PersistencyDataBasic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App_TableView_PersistencyDataBasic
+//
+//  Created by User on 12/1/16.
+//  Copyright © 2016 iCologic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
